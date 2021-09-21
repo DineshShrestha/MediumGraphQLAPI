489a15231aeb10566f8690207dc920eff06dc6f4
Dinesh Shrestha <dinu.shrestha777@gmail.com>
2021-09-21 21:31:22 +0200

    Modified the router.ex file to fix the error

--- lib/medium_graphql_api_web/router.ex
+++ lib/medium_graphql_api_web/router.ex
@@ -1,33 +1,18 @@
+
 defmodule MediumGraphqlApiWeb.Router do
   use MediumGraphqlApiWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api" do
-    pipe_through :api
+    pipe_through(:api)
 
     forward("/graphql", Absinthe.Plug, schema: MediumGraphqlApiWeb.Schema)
 
-    if mix.env() == :dev do
-      forward("/graphiql", Absinthe.Plug.GraphicQL, schema: MediumGraphqlApiWeb.Schema)
-    end
-  end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-      live_dashboard "/dashboard", metrics: MediumGraphqlApiWeb.Telemetry
+    if Mix.env() == :dev do
+      forward("/graphiql", Absinthe.Plug.GraphiQL, schema: MediumGraphqlApiWeb.Schema)
     end
   end
 end
